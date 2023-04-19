f588dbc1d8a03158e9c3e7a44b69c9f98fcfa0b6
Gary Larner <LAR86200@pennwest.edu>
2023-04-18 23:55:56 -0400

    Ver1

--- Turtle Graphics.cpp
+++ Turtle Graphics.cpp
@@ -59,19 +59,18 @@ int Options() {
     //print----------------------------------------
     cout << "1 to display board" << endl;
     cout << "2 to save board" << endl;
-    cout << "3 to save instructions" << endl;
-    cout << "4 to load instructions" << endl;
-    cout << "5 to toggle the pen" << endl;
-    cout << "6 to move forward" << endl;
-    cout << "7 to rotate counterclockwise" << endl;
-    cout << "8 to rotate clockwise" << endl;
-    cout << "9 to reset the turtle" << endl;
-    cout << "10 to end the program" << endl;
+    cout << "3 to load instructions" << endl;
+    cout << "4 to toggle the pen" << endl;
+    cout << "5 to move forward" << endl;
+    cout << "6 to rotate counterclockwise" << endl;
+    cout << "7 to rotate clockwise" << endl;
+    cout << "8 to reset the turtle" << endl;
+    cout << "9 to end the program" << endl;
     //---------------------------------------------
     //Get user input---------------------------------------------
-    while (!(cin.fail()) && selection < 0 || selection > 10) {
+    while (!(cin.fail()) && selection < 0 || selection > 9) {
         cin >> selection;
-        if (cin.fail() || selection < 0 || selection > 10) {
+        if (cin.fail() || selection < 0 || selection > 9) {
             cout << "Invalid, please try again." << endl;
         }
     }
@@ -79,7 +78,7 @@ int Options() {
     return selection;
 }
 void Rotate(int rotBy) {
-    //passes in a number and adds it to the turtle face (class?) variable
+    //passes in a number and adds it to the turtle face variable
     turtleFace += rotBy;
     //check for overflow----------- 
     if (turtleFace == 4) {
@@ -93,56 +92,78 @@ void Rotate(int rotBy) {
 }
 
 void MoveForward(char ppBoard[HEIGHT][WIDTH], bool ppPen, int ppTurtLOC[2]) {
-    bool validCheck = true;
+    //supposed to end the while loop when a successful iteration happens
+    bool exitCheck = true;
+    //holds the distance the user wants to move
     int distance = 0;
-    while (validCheck) {
+    //keep going until valid input
+    while (cin.fail() || exitCheck) {
+        cin.clear();
+        cin.ignore(255, '\n');
         cout << "How far do you want to move? " << endl;
         cin >> distance;
-        
+        switch (turtleFace) {
+        case 0: //east
+            //check if the turtle can go that far--------
+            if (WIDTH - ppTurtLOC[0] > distance) {
+                ppTurtLOC[0] += distance;
+                exitCheck = false; //exits the while loop
+            }
+            else {
+                cout << "That is too far given the direction" << endl;
+            }
+            //-------------------------------------------
+            break;
+        case 1: //south
+
+            break;
+        case 2: //west
+
+            break;
+        case 3: //north
+
+            break;
+        }
     }
 }
-void processRouter(int pUserSel, char pBoard[HEIGHT][WIDTH], bool& pPen, int pTurtLOC[2]) {
+bool processRouter(int pUserSel, char pBoard[HEIGHT][WIDTH], bool& pPen, int pTurtLOC[2]) {
     //compare the selection from the user to actions in the switch
     switch (pUserSel)
     {
     case 1: //draw board
         DisplayBoard(pBoard, pPen);
-        break;
+        return true;
     case 2: //save the board and descriptions to file
         //SaveBoard(pBoard);
-        break;
-    case 3: //save the instructions to file
-        //SaveInstructions();
-        break;
-    case 4: //load instructions from file
+        return true;
+    case 3: //load instructions from file
         //LoadInstructions();
-        break;
-    case 5: //toggle the pen
+        return true;
+    case 4: //toggle the pen
         pPen = !(pPen);
-        break;
-    case 6: //move the turtle forward, drawing or not drawing based on the pen
+        return true;
+    case 5: //move the turtle forward, drawing or not drawing based on the pen
         MoveForward(pBoard, pPen, pTurtLOC);
-        break;
-    case 7: //rotate counterclockwise
+        return true;
+    case 6: //rotate counterclockwise
         Rotate(-1);
-        break;
-    case 8: //rotate clockwise
+        return true;
+    case 7: //rotate clockwise
         Rotate(1);
-        break;
-    case 9: //move the turtle to 0,0 and clear the board
+        return true;
+    case 8: //move the turtle to 0,0 and clear the board
         BuildBoard(pBoard);
         pTurtLOC[0] = 0;
         pTurtLOC[1] = 0;
         pPen = 0;
-        break;
-    case 10: //end the program
-        exit(0);
-        break;
+        return true;
+    case 9: //end the program
+        return false;
     default: //shouldn't do anything, but just in *case*
         cout << "Something went wrong!" << endl;
-        break;
+        return true;
     }
-    return;
+    
 }
 
 
@@ -150,6 +171,7 @@ void processRouter(int pUserSel, char pBoard[HEIGHT][WIDTH], bool& pPen, int pTu
 int main() {
     //Declare variables----------
     bool penTgl = false; //whether the pen is up or down. false is up
+    bool keepGoing = true; //whether the process keeps going
     int turtleLOC[2] = { 0,0 }; //represents the location of the turtle in the board
     char board[HEIGHT][WIDTH]; //board, what else?
     int userSel = 0; //Just initializing which option the user will select
@@ -162,9 +184,9 @@ int main() {
   
 
     //Infinite loop that exits when the user uses 10, I noticed many graphical apps use this method, such as Tkninter for Python
-    while (true) {
+    while (keepGoing) {
         userSel = Options(); //gets the the command the user wants
-        processRouter(userSel, board, penTgl, turtleLOC); //decides which functions to call
+        keepGoing = processRouter(userSel, board, penTgl, turtleLOC); //decides which functions to call
     }
 
 
